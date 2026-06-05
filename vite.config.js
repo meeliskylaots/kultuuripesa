4cc74187b53fdd8b9703832f5c8c93d8757efa63
meeliskylaots <meeliskylaots@gmail.com>
2026-06-05 22:28:08 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Relative base makes the project work on any GitHub Pages repository path.
-  base: './'
-})
